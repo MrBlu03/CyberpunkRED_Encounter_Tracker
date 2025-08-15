6c81649b3a738bb344a6a163640d739e68ed71ba
MrBlu03 <96666159+MrBlu03@users.noreply.github.com>
2025-08-15 21:12:37 +0200

    netlify

--- gm-tool/vite.config.ts
+++ gm-tool/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CyberpunkRED_Encounter_Tracker/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
